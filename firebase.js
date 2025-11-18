b7b9cd430d6bd74478ea86a730ca837e0a933ef4
truben02 <truben02@gmail.com>
2025-11-18 17:31:10 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,14 @@
-<!-- Firebase App -->
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"></script>
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDT8jdl9qpUllaItXbPfV0EzLBxATOWqeI",
+  authDomain: "arisahealth-scheduler.firebaseapp.com",
+  databaseURL: "https://arisahealth-scheduler-default-rtdb.firebaseio.com",
+  projectId: "arisahealth-scheduler",
+  storageBucket: "arisahealth-scheduler.firebasestorage.app",
+  messagingSenderId: "208760160482",
+  appId: "1:208760160482:web:ce6a72f2cae6c29d369ebb"
+};
 
-<!-- Firebase Database -->
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-database-compat.js"></script>
-
-<script>
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDT8jdl9qpUllaItXbPfV0EzLBxATOWqeI",
-    authDomain: "arisahealth-scheduler.firebaseapp.com",
-    databaseURL: "https://arisahealth-scheduler-default-rtdb.firebaseio.com",
-    projectId: "arisahealth-scheduler",
-    storageBucket: "arisahealth-scheduler.firebasestorage.app",
-    messagingSenderId: "208760160482",
-    appId: "1:208760160482:web:ce6a72f2cae6c29d369ebb"
-  };
-
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  const db = firebase.database();
-</script>
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
